e78cac08cf5bd740d58c51a73f4db1d86f940add
Devin Chalmers <devin.chalmers@gmail.com>
2010-07-26 12:14:43 -0400

    Fixing warnings -- I don't care about iOS2

--- src/FBDialog.m
+++ src/FBDialog.m
@@ -325,11 +325,7 @@ static CGFloat kBorderWidth = 10;
     [_closeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
     [_closeButton addTarget:self action:@selector(cancel)
       forControlEvents:UIControlEventTouchUpInside];
-	if ([_closeButton respondsToSelector:@selector(titleLabel)]) {
-		_closeButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
-	} else { // This triggers a deprecation warning but at least it will work on OS 2.x
-		_closeButton.font = [UIFont boldSystemFontOfSize:12];
-	}
+    _closeButton.titleLabel.font = [UIFont boldSystemFontOfSize:12];
 	_closeButton.showsTouchWhenHighlighted = YES;
     _closeButton.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin
       | UIViewAutoresizingFlexibleBottomMargin;
